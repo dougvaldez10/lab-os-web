4753948295eeed4c346a8aa7dae4dd873430c6cd
Douglas Valdez <dougvaldez10@gmail.com>
2026-03-25 15:57:28 -0700

    Fix dead import causing App Error

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useRef } from "react";
 import { Bell, Search, Star, MessageSquare, Clipboard, MoreHorizontal, LogOut, ChevronDown, Check, RefreshCw, Layers, Smile, Shield, Smartphone, Package, Target, Sun, X, Calculator, DollarSign, Percent } from "lucide-react";
-import { verifyPin, getAllUsers, loginUser } from "@/lib/auth";
+import { getAllUsers, loginUser } from "@/lib/auth";
 import { generateReceipt } from "@/app/actions/receipts";
 import { Toaster, toast } from 'sonner';
 import { supabase } from '@/lib/supabase';
