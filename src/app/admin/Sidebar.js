24a471114e262fd8bd89805784b67e0bc2357f2d
Doug Valdez <dougvaldez10@gmail.com>
2026-06-03 15:58:10 -0700

    Revert Lab logo color to gold

--- src/app/admin/Sidebar.js
+++ src/app/admin/Sidebar.js
@@ -65,7 +65,7 @@ export default function Sidebar() {
           />
           {!isCollapsed && (
             <h2 className="text-xl font-black text-white tracking-tight whitespace-nowrap animate-fade-in">
-              <span style={{ color: 'red' }}>Lab</span> OS
+              <span className="text-[#D4AF37]">Lab</span> OS
             </h2>
           )}
         </div>
